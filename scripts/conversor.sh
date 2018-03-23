a59e258df31f6a1cdfff6d916dc8c33f6892da55
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-03-23 17:45:57 -0300

    Commot inicial

---
+++ scripts/conversor.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+
+HOME_SCRIPTS='/home/linuxmint/git/AluraCursoShellScrip/scripts'; export HOME_SCRIPTS
+HOME_IMAGES='/home/linuxmint/git/AluraCursoShellScrip/imagens-livros'; export HOME_IMAGES
+
+convert $HOME_IMAGES/$1.jpg $HOME_IMAGES/$1.png
